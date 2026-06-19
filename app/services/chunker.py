4bc8f4cf8eaead07153cba89f453f8359f6f32e0
“Crawford <crawfordclarke123@gmail.com>
2026-06-19 16:49:54 +0100

    feat: chunk indexing function

---
+++ app/services/chunker.py
@@ -0,0 +1,22 @@
+
+
+def group_list(num_sentences):
+    current_start = 0
+    gpl = []
+    if num_sentences < 3:
+        return [(i,) for i in range(num_sentences)]
+    while num_sentences - current_start >= 3:
+        temp_list = [current_start, current_start + 1, current_start + 2]
+        tupled_list = tuple(temp_list)
+        gpl.append(tupled_list)
+        current_start += 2
+    remaining_sentences = num_sentences - current_start - 1    
+    if remaining_sentences == 1 and len(gpl) > 0:
+        last_group = gpl.pop()
+        gpl.append((last_group[0],last_group[1],last_group[2], current_start + 1))
+    if remaining_sentences == 2 and len(gpl) > 0:
+        last_group = gpl.pop()
+        gpl.append((last_group[0],last_group[1],current_start + 1, current_start + 2))
+    return gpl
+
+print(group_list(1))
