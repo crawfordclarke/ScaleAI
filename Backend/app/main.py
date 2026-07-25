316bd13a0cd788b2e2a0da80e1b991c1525d8c81
“Crawford <crawfordclarke123@gmail.com>
2026-07-25 16:18:08 +0100

    refactor: restructure repo into Backend/ and Frontend/ monorepo

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from Backend.app.routes.fight import fight, router as fight_router
 from Backend.app.routes.characters import router as character_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(fight_router)
 app.include_router(character_router)
 
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
